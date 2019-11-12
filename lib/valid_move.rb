fd60e00fef2d8098c04b98dce0f72d75b05a3e43
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-12 18:55:29 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,17 @@
 # code your #valid_move? method here
-
+def valid_move? (board, position)
+  if ((position <= board.length - 1) && !position_taken?(board, position))
+    return true
+  else
+    return false
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken? (board, position)
+  if (board[position] == 'X' || board[position] == 'O')
+    return true
+  else
+    return false
+  end
+end
